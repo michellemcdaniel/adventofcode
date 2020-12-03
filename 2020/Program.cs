c3c136f252e6e3b51a855f1a634904cc2d03aab8
Michelle McDaniel <adiaaida@gmail.com>
2020-12-03 10:18:34 -0800

    Error on missing day

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -6,6 +6,12 @@ namespace adventofcode
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("You must supply the number of the day to run.");
+                return;
+            }
+            
             int day = Convert.ToInt32(args[0]);
 
             switch (day)
