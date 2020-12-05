502ad16bd22a9740c36c199c827fa090273e9198
adiaaida <adiaaida@gmail.com>
2020-12-05 07:50:10 -0800

    Simplify day 5 even more

--- 2020/Day05.cs
+++ 2020/Day05.cs
@@ -14,38 +14,21 @@ namespace adventofcode
 
             int maxId = 0;
             int minId = 128*8+7;
-            List<int> emptySeats = new List<int>();
-
-            for (int i = 0; i < 128; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    emptySeats.Add(i*8+j);
-                }
-            }
+            List<int> emptySeats = new List<int>(Enumerable.Range(0,128*8));
 
             foreach(string seat in input)
             {
                 string seatInBinary = seat.Replace('F', '0').Replace('B','1').Replace('L','0').Replace('R','1');
-                string row = new string(seatInBinary.Take(7).ToArray());
-                string column = new string(seatInBinary.Skip(7).Take(3).ToArray());
 
-                int id = Convert.ToInt32(row, 2)*8+Convert.ToInt32(column, 2);
-                maxId = id > maxId ? id : maxId;
-                minId = id < minId ? id : minId;
+                int id = Convert.ToInt32(seatInBinary, 2);
+                maxId = Math.Max(id, maxId);
+                minId = Math.Min(id, minId);
 
                 emptySeats.Remove(id);
             }
 
             Console.WriteLine($"Max ID: {maxId}");
-
-            foreach(int id in emptySeats.Where(s => s < maxId && s > minId))
-            {
-                if (!emptySeats.Contains(id-1) && !emptySeats.Contains(id+1))
-                {
-                    Console.WriteLine($"Potential Id: {id}");
-                }
-            }
+            Console.WriteLine($"Potential Id: {emptySeats.Where(s => s < maxId && s > minId).First()}");
         }
     }
 }
