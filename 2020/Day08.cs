f5388b8cf3277ba1833f1574eb97c3f2b605ba9b
adiaaida <adiaaida@gmail.com>
2020-12-07 21:59:15 -0800

    Final cleanup

--- 2020/Day08.cs
+++ 2020/Day08.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -11,11 +12,9 @@ namespace adventofcode
         public static void Execute()
         {
             List<string> input = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "input", "day08.txt")).ToList();
-
             int accumulator = 0;
 
             CheckProgram(input, out accumulator);
-
             Console.WriteLine($"Part 1: {accumulator}");
 
             for (int i = 0; i < input.Count(); i++)
@@ -23,54 +22,50 @@ namespace adventofcode
                 string original = input[i];
                 input[i] = input[i].Contains("nop") ? input[i].Replace("nop", "jmp") : input[i].Replace("jmp", "nop");
                 
-                if (!CheckProgram(input, out accumulator))
+                if (CheckProgram(input, out accumulator))
                 {
+                    Console.WriteLine($"Part 2: {accumulator}");
                     break;
                 }
 
                 input[i] = original;
-            }  
-            
-            Console.WriteLine($"Part 2: {accumulator}");
+            }
         }
 
         public static bool CheckProgram(List<string> input, out int accumulator)
         {
             HashSet<int> lineHit = new HashSet<int>();
-            bool infinite = false;
-            int currentLine = 0;
+            int instructionPointer = 0;
             accumulator = 0;
 
-            while (!infinite && currentLine < input.Count())
+            while (instructionPointer < input.Count())
             {
-                if (lineHit.Contains(currentLine))
+                if (!lineHit.Add(instructionPointer))
                 {
-                    infinite = true;
-                    break;
+                    return false;
                 }
 
-                lineHit.Add(currentLine);
-                string[] lineSplit = input[currentLine].Split(" ");
+                string[] lineSplit = input[instructionPointer].Split(" ");
                 int value = int.Parse(lineSplit[1]);
 
                 switch (lineSplit[0])
                 {
                     case "acc":
                         accumulator += value;
-                        currentLine++;
+                        instructionPointer++;
                         break;
                     case "jmp":
-                        currentLine += value;
+                        instructionPointer += value;
                         break;
                     case "nop":
-                        currentLine++;
+                        instructionPointer++;
                         break;
                     default:
                         break;
                 }
             }
 
-            return infinite;
+            return true;
         }
     }
 }
