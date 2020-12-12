c7c62fce5f134a203592944350cf2387867836ea
adiaaida <adiaaida@gmail.com>
2020-12-12 13:36:02 -0800

    Simplify day 12

--- 2020/solutions/Day12.cs
+++ 2020/solutions/Day12.cs
@@ -13,80 +13,58 @@ namespace adventofcode
         {
             List<string> input = File.ReadAllLines(filename).ToList();
 
-            int direction = 0;
-            List<char> directions = new List<char>(){'E','S','W','N'};
-
-            int x = 0;
-            int y = 0;
-
-            int waypointX = 10;
-            int waypointY = 1;
+            List<(bool, int, int)> moveWayPoints = new List<(bool, int, int)>() { (false, 1, 0), (true, 10, 1) };
 
             // Part One
-            foreach(var line in input)
+            foreach (var waypoint in moveWayPoints)
             {
-                char letter = line[0];
-                int number = int.Parse(line[1..]);
+                (bool moveWayPoint, int waypointX, int waypointY) = waypoint;
+                int x = 0;
+                int y = 0;
 
-                if (letter == 'F')
+                foreach(var line in input)
                 {
-                    (x,y) = MoveDirection(direction, x, y, number);
-                }
-                else if (letter == 'R' || letter == 'L')
-                {
-                    int adjusted = letter == 'R' ? number : 360-number;
-                    direction = (direction+adjusted/90)%4;
-                }
-                else
-                {
-                    (x,y) = MoveDirection(directions.IndexOf(letter), x, y, number);
-                }
-            }
+                    char letter = line[0];
+                    int number = int.Parse(line[1..]);
 
-            Console.WriteLine($"Part One: {Math.Abs(x)+Math.Abs(y)}");
-
-            x = 0;
-            y = 0;
-
-            // Part Two
-            foreach(var line in input)
-            {
-                char letter = line[0];
-                int number = int.Parse(line[1..]);
-
-                if (letter == 'F')
-                {
-                    x += number*waypointX;
-                    y += number*waypointY;
-                }
-                else if (letter == 'R' || letter == 'L')
-                {
-                    int adjusted = letter == 'R' ? number : 360-number;
-                    for(int rotation = adjusted/90; rotation > 0; rotation--)
+                    if (letter == 'F')
                     {
-                        int swap = waypointX;
-                        waypointX = waypointY;
-                        waypointY = -swap;
+                        x += number*waypointX;
+                        y += number*waypointY;
+                    }
+                    else if (letter == 'R' || letter == 'L')
+                    {
+                        int adjusted = letter == 'R' ? number : 360-number;
+                        for(int rotation = adjusted/90; rotation > 0; rotation--)
+                        {
+                            int swap = waypointX;
+                            waypointX = waypointY;
+                            waypointY = -swap;
+                        }
+                    }
+                    else if (moveWayPoint)
+                    {
+                        (waypointX, waypointY) = MoveDirection(letter, waypointX, waypointY, number);
+                    }
+                    else
+                    {
+                        (x, y) = MoveDirection(letter, x, y, number);
                     }
                 }
-                else
-                {
-                    (waypointX, waypointY) = MoveDirection(directions.IndexOf(letter), waypointX, waypointY, number);
-                }
-            }
 
-            Console.WriteLine($"Part Two: {Math.Abs(x)+Math.Abs(y)}");
+                Console.WriteLine($"Distance moved: {Math.Abs(x)+Math.Abs(y)}");
+            }
         }
 
-        public static (int, int) MoveDirection(int direction, int x, int y, int number)
+        public static (int, int) MoveDirection(char direction, int x, int y, int number)
         {
-            if (direction == 1 || direction == 2)
+            if (direction == 'W' || direction == 'S')
             {
                 number = -number;
             }
 
-            int xChange = direction%2 == 0 ? number : 0;
-            int yChange = direction%2 != 0 ? number : 0;
+            int xChange = direction == 'E' || direction == 'W' ? number : 0;
+            int yChange = direction == 'S' || direction == 'N' ? number : 0;
 
             return (x + xChange, y + yChange);
         }
