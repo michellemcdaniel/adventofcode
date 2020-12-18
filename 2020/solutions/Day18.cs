897d30368f04627e50eb89509b07a169024b9064
Michelle McDaniel <adiaaida@gmail.com>
2020-12-18 15:21:59 -0800

    Redo day 18, with a stack.

--- 2020/solutions/Day18.cs
+++ 2020/solutions/Day18.cs
@@ -12,28 +12,26 @@ namespace adventofcode
         {
             List<string> input = File.ReadAllLines(filename).ToList();
 
-            long noPrecedence = 0;
-            long precedence = 0;
             long withGrammarNoPrecedence = 0;
             long withGrammarPrecedence = 0;
-            long withGrammarRegularPrecedence = 0;
+
+            long withStackNoPrecedence = 0;
+            long withStackAddPrecedence = 0;
 
             foreach(var i in input)
             {
-                noPrecedence+= DoMath(i.Split(" "));
-                precedence+= DoMathPrecedence(i.Split(" "));
                 withGrammarNoPrecedence += Evaluate(i, Precedence.NoPrecedence);
                 withGrammarPrecedence += Evaluate(i, Precedence.SwappedPrecedence);
-                withGrammarRegularPrecedence += Evaluate(i, Precedence.RegularPrecedence);
+
+                withStackNoPrecedence += StackMath(i, Precedence.NoPrecedence);
+                withStackAddPrecedence += StackMath(i, Precedence.SwappedPrecedence);
             }
-            
-            Console.WriteLine($"Part one: {noPrecedence}");
-            Console.WriteLine($"Part two: {precedence}");
 
-            Console.WriteLine($"Part one: {withGrammarNoPrecedence}");
-            Console.WriteLine($"Part two: {withGrammarPrecedence}");
+            Console.WriteLine($"Part one, grammar: {withGrammarNoPrecedence}");
+            Console.WriteLine($"Part two, grammar: {withGrammarPrecedence}");
 
-            Console.WriteLine($"Part none: {withGrammarRegularPrecedence}");
+            Console.WriteLine($"Part one, stack: {withStackNoPrecedence}");
+            Console.WriteLine($"Part two, stack: {withStackAddPrecedence}");
         }
 
         public static long Evaluate(string input, Precedence precedence)
@@ -45,165 +43,116 @@ namespace adventofcode
             return root.Evaluate();
         }
 
-        public static long DoMathPrecedence(string[] expression)
+        public static long StackMath(string expression, Precedence precedence)
         {
-            int bracketCounter = 0;
-            int operatorIndex = -1;
+            Stack<Operation> math = new Stack<Operation>();
+            math.Push(new Operation());
 
-            for(int i = 0; i < expression.Length; i++)
+            foreach(char c in expression.Replace(" ", ""))
             {
-                string c = expression[i];
-                if (c == "(") bracketCounter++;
-                else if (c == ")") bracketCounter--;
-                else if ((c == "*") && bracketCounter == 0)
+                switch (c)
                 {
-                    // Multiplication has the least precedence. Once we find it, break.
-                    operatorIndex = i;
-                    break;
-                }
-                else if (c == "+" && bracketCounter == 0 && operatorIndex < 0)
-                {
-                    // We found addition, but since it has higher precedence than
-                    // multiplication, let's go see if we can find a multiplication first
-                    // If we don't, we'll multiply. That's fine too
-                    operatorIndex = i;
+                    case '+':
+                    case '*':
+                        math.Peek().Operators.Add(c);
+                        break;
+                    case '(':
+                        math.Push(new Operation());
+                        break;
+                    case ')':
+                        long total = 0;
+                        switch (precedence)
+                        {
+                            case Precedence.NoPrecedence:
+                                total = math.Pop().Calculate();
+                                break;
+                            case Precedence.SwappedPrecedence:
+                                total = math.Pop().CalculateWithAddPrecedence();
+                                break;
+                            default: 
+                                throw new ArgumentException($"{precedence} is not a recognized precedence", "precedence");
+                        }
+                        math.Peek().Values.Add(total);
+                        break;
+                    default:
+                        long value = long.Parse(c.ToString());
+                        math.Peek().Values.Add(value);
+                        break;
                 }
             }
 
-            if (operatorIndex < 0)
+            switch (precedence)
             {
-                if (expression[0] == "(" && expression[expression.Length-1] == ")")
-                {
-                    // Get rid of those pesky parens and do the math over.
-                    int end = expression.Length-1;
-                    return DoMathPrecedence(expression[1..end]);
-                }
-                
-                // Rejoice, for we have found our end number.
-                return long.Parse(expression.First());
-            }
-
-            else
-            {
-                // Do the math. So much easier with precendence
-                int second = operatorIndex+1;
-                if (expression[operatorIndex] == "+")
-                {
-                    return DoMathPrecedence(expression[0..operatorIndex]) + DoMathPrecedence(expression[second..]);
-                }
-                else
-                {
-                    return DoMathPrecedence(expression[0..operatorIndex]) * DoMathPrecedence(expression[second..]);
-                }
+                case Precedence.NoPrecedence:
+                    return math.Peek().Calculate();
+                case Precedence.SwappedPrecedence:
+                    return math.Peek().CalculateWithAddPrecedence();
+                default: 
+                    throw new ArgumentException($"{precedence} is not a recognized precedence", "precedence");
             }
         }
-        
-        public static long DoMath(string[] expression)
+
+        public class Operation
         {
-            int bracketCounter = 0;
-            int lastCloseBracket = 0;
-            int operatorIndex = -1;
+            public List<char> Operators {get;set;}
+            public List<long> Values {get;set;}
 
-            for(int i = 0; i < expression.Length; i++)
+            public Operation()
             {
-                string c = expression[i];
-                if (c == "(") bracketCounter++;
-                else if (c == ")") bracketCounter--;
-                else if ((c == "*" || c == "+") && bracketCounter == 0)
-                {
-                    // We found our first operator. We can gtfo now.
-                    operatorIndex = i;
-                    break;
-                }
+                Operators = new List<char>();
+                // Each operator starts with a + so that calculate works properly.
+                Operators.Add('+');
+                Values = new List<long>();
             }
 
-            if (operatorIndex < 0)
+            public long Calculate()
             {
-                if (expression[0] == "(" && expression[expression.Length-1] == ")")
+                long total = 0;
+                for (int i = 0; i < Values.Count(); i++)
                 {
-                    // We found a paren range. Remove the parens, rejoice.
-                    int end = expression.Length-1;
-                    return DoMath(expression[1..end]);
-                }
-                
-                // We have a single number. Return it. The recursion is over.
-                return long.Parse(expression.First());
-            }
-
-            else
-            {
-                if (operatorIndex == 1)
-                {
-                    long rightSide = long.Parse(expression[0]);
-                    
-                    if (expression[operatorIndex+1] != "(")
+                    if (Operators[i] == '+')
                     {
-                        // if the next token is not an open paren, it can only be a number, so do the math
-                        // and then paste the math back onto the expression
-                        List<string> newExpression = new List<string>();
-                        if (expression[operatorIndex] == "+")
-                        {
-                            newExpression.Add($"{rightSide+int.Parse(expression[2])}");
-                        }
-                        else
-                        {
-                            newExpression.Add($"{rightSide*int.Parse(expression[2])}");
-                        }
-
-                        newExpression.AddRange(expression[3..]);
-                        return DoMath(newExpression.ToArray());
+                        total += Values[i];
                     }
                     else
                     {
-                        // if the next token is a paren, we need to do that stuff first, so find the end of
-                        // the paren section
-                        int start = operatorIndex+1;
-                        bracketCounter = 0;
-                        for(int i = start; i < expression.Length; i++)
-                        {
-                            string c = expression[i];
-                            if (c == "(") bracketCounter++;
-                            else if (c == ")") bracketCounter--;
-                            if (bracketCounter == 0)
-                            {
-                                // don't really know why it needs to be + 1, but it does?
-                                lastCloseBracket = i+1;
-                                break;
-                            }
-                        }
-
-                        // Paste the math of the right side combined with the expression in the parens
-                        // back onto everything that comes after the parens.
-                        List<string> newExpression = new List<string>();
-                        if (expression[operatorIndex] == "+")
-                        {
-                            newExpression.Add($"{rightSide + DoMath(expression[start..lastCloseBracket])}");
-                        }
-                        else
-                        {
-                            newExpression.Add($"{rightSide * DoMath(expression[start..lastCloseBracket])}");
-                        }
-
-                        int newStart = lastCloseBracket;
-                        newExpression.AddRange(expression[newStart..]);
-                        return DoMath(newExpression.ToArray());
+                        total *= Values[i];
                     }
                 }
-                else
-                {
-                    // It was not a number to start, but a thing in parens, so we need to do the paren
-                    // math. Luckily, we can use the operatorIndex as the end of the paren range,
-                    // since it was the first operator after all the parens closed. So do that math,
-                    // and then paste that value back in front of the list, along with the operator. Means we will 
-                    // have to parse this operation again, but c'est la vie.
-                    int start = operatorIndex+1;
+                return total;
+            }
 
-                    List<string> newExpression = new List<string>();
-                    newExpression.Add($"{DoMath(expression[0..operatorIndex])}");
-                    newExpression.Add(expression[operatorIndex]);
-                    newExpression.AddRange(expression[start..]);
-                    return DoMath(newExpression.ToArray());
+            public long CalculateWithAddPrecedence()
+            {
+                Stack<long> stack = new Stack<long>();
+                stack.Push(0);
+                long currentVal = -1;
+
+                for (int i = 0; i < Values.Count(); i++)
+                {
+                    if (Operators[i] == '+')
+                    {
+                        currentVal = stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(Values[i]);
+                    }
+                    if (currentVal != -1)
+                    {
+                        stack.Push(currentVal + Values[i]);
+                        currentVal = -1;
+                    }
                 }
+
+                long total = 1;
+
+                while(stack.Any())
+                {
+                    total*=stack.Pop();
+                }
+
+                return total;
             }
         }
     }
