c6b5cfed1b79af048bc5646915bd1ca727c8f90a
adiaaida <adiaaida@gmail.com>
2020-12-20 22:16:52 -0800

    Remove unnecessary usings

--- 2020/solutions/Day11.cs
+++ 2020/solutions/Day11.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace adventofcode
 {
