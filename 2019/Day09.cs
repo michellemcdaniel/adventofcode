cbe931e23cc3c7196d3da84d52cf9c6053eb49f8
adiaaida <adiaaida@gmail.com>
2020-12-25 09:24:37 -0800

    Add day 9 part 1

--- 2019/Day09.cs
+++ 2019/Day09.cs
@@ -13,12 +13,14 @@ namespace adventofcode
 
             string inputString = string.Join("", input);
 
+            IntCode partOne = new IntCode(inputString.Split(",").ToList().Select(o => long.Parse(o)).ToArray());
+            long result = partOne.Compute(1);
+            Console.WriteLine($"Part One: {result}");
+
             IntCode intCode = new IntCode(inputString.Split(",").ToList().Select(o => long.Parse(o)).ToArray());
+            result = intCode.Compute(2);
 
-            long result = intCode.Compute(2);
-
-            Console.Write($"{result}");
-            Console.WriteLine();
+            Console.WriteLine($"Part Two: {result}");
         }
     }
 }
