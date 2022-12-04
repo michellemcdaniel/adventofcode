cef8d72703a46371ed74fae1b5983dd07cff358b
Michelle McDaniel <adiaaida@gmail.com>
2022-12-03 21:28:41 -0800

    Do 2022 day 4

--- src/AdventOfCode.2022/solutions/Day04.cs
+++ src/AdventOfCode.2022/solutions/Day04.cs
@@ -9,8 +9,30 @@ namespace AdventOfCode.TwentyTwo
     {
         public static void Execute(string filename)
         {
-            Console.WriteLine($"Part one: ");
-            Console.WriteLine($"Part two: ");
+            int fullyContains = 0;
+            int overlap = 0;
+            foreach(var line in File.ReadLines(filename))
+            {
+                var pair = line.Split(",").Select(s => s.Split("-").Select(e => int.Parse(e)));
+                var elfOneRange = Enumerable.Range(pair.First().First(), pair.First().Last() - pair.First().First() + 1);
+                var elfTwoRange = Enumerable.Range(pair.Last().First(), pair.Last().Last() - pair.Last().First() + 1);
+
+                var oneIntersectTwo = elfOneRange.Intersect(elfTwoRange);
+                var twoIntersectOne = elfTwoRange.Intersect(elfOneRange);
+
+                if (oneIntersectTwo.Count() == elfTwoRange.Count() || twoIntersectOne.Count() == elfOneRange.Count())
+                {
+                    fullyContains++;
+                }
+
+                if (oneIntersectTwo.Any() || twoIntersectOne.Any())
+                {
+                    overlap++;
+                }
+            }
+
+            Console.WriteLine($"Part one: {fullyContains}");
+            Console.WriteLine($"Part two: {overlap}");
         }
     }
 }
