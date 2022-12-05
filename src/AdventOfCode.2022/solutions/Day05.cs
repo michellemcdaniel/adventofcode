d4cb8b59a21da0a7deadb1f2d600d57ff63895ef
Michelle McDaniel <adiaaida@gmail.com>
2022-12-04 21:39:42 -0800

    2022 day 5
    it's ugly and i don't care.

--- src/AdventOfCode.2022/solutions/Day05.cs
+++ src/AdventOfCode.2022/solutions/Day05.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Linq;
+using AdventOfCode.Helpers;
 
 namespace AdventOfCode.TwentyTwo
 {
@@ -9,8 +11,64 @@ namespace AdventOfCode.TwentyTwo
     {
         public static void Execute(string filename)
         {
-            Console.WriteLine($"Part one: ");
-            Console.WriteLine($"Part two: ");
+            string[] allLines = File.ReadAllLines(filename);
+
+            List<string> stacks = new();
+            List<string> stacks9001 = new();
+            List<string> instructions = new();
+            bool change = false;
+
+            foreach(var line in allLines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    change = true;
+                    continue;
+                }
+
+                if (change)
+                {
+                    instructions.Add(line);
+                }
+                else
+                {
+                    stacks.Add(line);
+                    stacks9001.Add(line);
+                }
+            }
+
+            foreach (var instruction in instructions)
+            {
+
+                RegexHelper.Match(instruction, @"move (\d+) from (\d) to (\d)", out int count, out int from, out int to);
+                from--;
+                to--;
+
+                for (int i = 0; i < count; i++)
+                {
+                    char letter = stacks[from].Last();
+                    stacks[from] = stacks[from].Remove(stacks[from].Length - 1, 1);
+                    stacks[to] += letter;
+                }
+
+                string move = stacks9001[from].Substring(stacks9001[from].Length - count, count);
+                stacks9001[from] = stacks9001[from].Remove(stacks9001[from].Length - count, count);
+                stacks9001[to] += move;
+            }
+
+            string partOne = "";
+            string partTwo = "";
+            foreach (var stack in stacks)
+            {
+                partOne += stack.Last();
+            }
+            foreach (var stack in stacks9001)
+            {
+                partTwo += stack.Last();
+            }
+
+            Console.WriteLine($"Part one: {partOne}");
+            Console.WriteLine($"Part two: {partTwo}");
         }
     }
 }
