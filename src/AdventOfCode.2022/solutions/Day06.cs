013c910dcaec341e5b9bf1a2f44d84f5353b5595
Michelle McDaniel <adiaaida@gmail.com>
2022-12-05 21:14:59 -0800

    Add 2022 day 6

--- src/AdventOfCode.2022/solutions/Day06.cs
+++ src/AdventOfCode.2022/solutions/Day06.cs
@@ -9,8 +9,57 @@ namespace AdventOfCode.TwentyTwo
     {
         public static void Execute(string filename)
         {
-            Console.WriteLine($"Part one: ");
-            Console.WriteLine($"Part two: ");
+            Console.WriteLine($"Part one: {FindStart(File.ReadAllText(filename), 4)}");
+            Console.WriteLine($"Part two: {FindStart(File.ReadAllText(filename), 14)}");
+
+            Console.WriteLine($"Part one: {FindStartString(File.ReadAllText(filename), 4)}");
+            Console.WriteLine($"Part two: {FindStartString(File.ReadAllText(filename), 14)}");
+        }
+
+        private static int FindStart(string input, int count)
+        {
+            Queue<char> stream = new Queue<char>();
+            int position = 0;
+
+            foreach(char letter in input)
+            {
+                position++;
+                while (stream.Contains(letter))
+                {
+                    stream.Dequeue();
+                }
+
+                stream.Enqueue(letter);
+
+                if (stream.Count == count)
+                {
+                    return position;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int FindStartString(string input, int count)
+        {
+            string start = "";
+            int position = 0;
+
+            while (position < input.Length)
+            {
+                int index = start.IndexOf(input[position]);
+                if (index != -1)
+                {
+                    start = start.Substring(index+1);
+                }
+                start += input[position++];
+                
+                if (start.Length == count)
+                {
+                    return position;
+                }
+            }
+            return -1;
         }
     }
 }
