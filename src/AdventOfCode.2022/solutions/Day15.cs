110b6bfda64d2224896c9dbabe24ccd5b37db5e2
Michelle McDaniel <adiaaida@gmail.com>
2022-12-15 07:50:15 -0800

    Major clean up and refactor of part 15.
    Takes part 1 from 16s to < 1s and part 2 from 69s to ~3s. Probably could still be faster, but I'm happy with it.

--- src/AdventOfCode.2022/solutions/Day15.cs
+++ src/AdventOfCode.2022/solutions/Day15.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Helpers;
@@ -11,16 +12,19 @@ namespace AdventOfCode.TwentyTwo
     {
         public static void Execute(string filename)
         {
-            List<(int, int)> sensors = new();
             HashSet<(long, long)> beacons = new();
-
-            DictionaryMap<char> map = new();
             Dictionary<(int row, int col), int> manhattanDistances = new();
-            long minCol = long.MaxValue;
-            long maxCol = long.MinValue;
+            int searchRow = 0;
+            int maxSize = 0;
 
             foreach (var line in File.ReadLines(filename))
             {
+                if (RegexHelper.Match(line, @"searchRow=\d+ maxSize=\d+"))
+                {
+                    RegexHelper.Match(line, @"searchRow=(\d+) maxSize=(\d+)", out searchRow, out maxSize);
+                    continue;
+                }
+
                 RegexHelper.Match(
                     line, 
                     @"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", 
@@ -28,95 +32,63 @@ namespace AdventOfCode.TwentyTwo
                     out int sensorRow, 
                     out int beaconCol, 
                     out int beaconRow);
-                
-                map.TryAdd((sensorRow, sensorCol), 'S');
-                sensors.Add((sensorRow, sensorCol));
-                map.TryAdd((beaconRow, beaconCol), 'B');
-                beacons.Add((beaconRow, beaconCol));
 
-                minCol = Math.Min(Math.Min(sensorCol, minCol), beaconCol);
-                maxCol = Math.Max(Math.Max(sensorCol, maxCol), beaconCol);
+                beacons.Add((beaconRow, beaconCol));
 
                 int manhattanDistance = Math.Abs(sensorRow-beaconRow)+Math.Abs(sensorCol-beaconCol);
                 manhattanDistances.Add((sensorRow, sensorCol), manhattanDistance);
             }
 
-            long searchRow = 2000000;
             HashSet<(long, long)> beaconSet = new();
 
-            // This is super slow, but tbh, I have no idea what to do about it.
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
             foreach(var distance in manhattanDistances)
             {
-                for (long i = minCol-distance.Value; i < maxCol+distance.Value; i++)
+                // Calculate "inverse" manhattan distance i.e.
+                // d = abs(row1-row2) + abs(col1-col2)
+                // d - (abs(row1-row2)) = abs(col1-col2)
+                // (+/-)range = col1-col2
+                // So everything between distance.Key.col-range and distance.Key.col+range cannot have a beacon
+                long range = distance.Value - Math.Abs(searchRow-distance.Key.row);
+                for (long i = distance.Key.col-range; i <= distance.Key.col+range; i++)
                 {
-                    if (beaconSet.Contains((searchRow,i)))
+                    if (beacons.Contains((searchRow, i)))
                     {
                         continue;
                     }
-                    else if (beacons.Contains((searchRow, i)))
-                    {
-                        continue;
-                    }
-                    if (Math.Abs(i - distance.Key.col)+Math.Abs(searchRow - distance.Key.row) <= distance.Value)
-                    {
-                        beaconSet.Add((searchRow,i));
-                    }
+                        
+                    beaconSet.Add((searchRow,i));
                 }
             }
 
-            Console.WriteLine($"Part one: {beaconSet.Count()}");
-            
-            HashSet<(long row, long col)> possible = new();
-            long max = 4000000;
+            timer.Stop();
+            Console.WriteLine($"Part one: {beaconSet.Count()} (time: {timer.ElapsedMilliseconds}ms)");
 
-            // Narrow down the search space a little, but it is still super slow
+            (long row, long col)? distressSignal = null;
+            timer.Restart();
+
             foreach (var distance in manhattanDistances)
             { 
+                // We want to check the range of points just beyond the diamonds created by the manhattan distance
+                // So we are basically searching a circle around the current signal point.
                 long row = 0;
                 long col = distance.Value;
-
-                while (row <= distance.Value && col >= 0)
+                
+                // Check the top half of the diamond
+                while (distressSignal == null && row <= distance.Value && col >= 0)
                 {
-                    CheckNeighbors(possible, distance, row, col);
-
-                    col--;
-                    row++;
+                    distressSignal = CheckNeighbors(manhattanDistances, distance, row++, col--, maxSize);
                 }
 
-                row = 0;
-                col = distance.Value;
-
-                // now check the down half
-                while (row >= -distance.Value && col >= 0)
+                if (distressSignal != null)
                 {
-                    CheckNeighbors(possible, distance, row, col);
-
-                    col--;
-                    row--;
-                }
-            }
-
-            foreach (var poss in possible)
-            {
-                if (poss.row < 0 || poss.col < 0 || poss.row > max || poss.col > max)
-                {
-                    continue;
-                }
-                bool found = true;
-                foreach(var distance in manhattanDistances)
-                {
-                    if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, poss.row, poss.col) <= distance.Value)
-                    {
-                        found = false;
-                        break;
-                    }
-                }
-                if (found)
-                {
-                    Console.WriteLine($"Part Two: {poss.row} {poss.col} = {poss.col*4000000 + poss.row}");
                     break;
                 }
             }
+
+            timer.Stop();
+            Console.WriteLine($"Part Two: {distressSignal?.col*4000000 + distressSignal?.row} (time: {timer.ElapsedMilliseconds}ms)");
         }
 
         public static long CalculateManhattanDistance(long sensorRow, long sensorCol, long beaconRow, long beaconCol)
@@ -124,39 +96,68 @@ namespace AdventOfCode.TwentyTwo
             return Math.Abs(sensorRow - beaconRow)+Math.Abs(sensorCol - beaconCol);
         }
 
-        public static void CheckNeighbors(HashSet<(long row, long col)> possible, KeyValuePair<(int row, int col), int> distance, long row, long col)
+        public static bool PointInBounds((long row, long col) point, (int min, int max) bounds)
         {
+            return point.row >= bounds.min && 
+                point.col >= bounds.min && 
+                point.row <= bounds.max && 
+                point.col <= bounds.max;
+        }
+
+        public static (long row, long col)? CheckNeighbors(
+            Dictionary<(int row, int col), int> signals, 
+            KeyValuePair<(int row, int col), int> distance, 
+            long row, 
+            long col, 
+            int maxSize)
+        {
+            HashSet<(long row, long col)> points = new();
+            (int, int)[] searchSpace = new (int,int)[]{(1,1), (-1,-1), (1,-1), (-1,1)};
+            
+            // Add all the possible points that we want to check.
+            // For the top and bottom of the diamond, we want to check up and down
+            // For all points, we want to check the left and right side of the points in both the left quadrant and right quadrant
             if (col == 0)
             {
-                // gotta check up and down
-                if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row+1,distance.Key.col) > distance.Value)
+                foreach ((int i, int j) in searchSpace)
                 {
-                    possible.Add((distance.Key.row+row+1, distance.Key.col));
-                }
-                if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row-1,distance.Key.col) > distance.Value)
-                {
-                    possible.Add((distance.Key.row+row-1, distance.Key.col));
+                    points.Add((distance.Key.row+(j*row)+i, distance.Key.col));
                 }
             }
 
-            // check left and right
-            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col+col+1) > distance.Value)
+            foreach((int i, int j) in searchSpace)
             {
-                possible.Add((distance.Key.row+row,distance.Key.col+col+1));
-            }
-            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col+col-1) > distance.Value)
-            {
-                possible.Add((distance.Key.row+row,distance.Key.col+col-1));
+                points.Add((distance.Key.row+(i*row),distance.Key.col+(i*col)+j));
+                points.Add((distance.Key.row-(i*row),distance.Key.col+(i*col)+j));
             }
 
-            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col-col+1) > distance.Value)
+            foreach(var point in points)
             {
-                possible.Add((distance.Key.row+row,distance.Key.col-col+1));
+                // First, check that the point is both in bounds and that the manhattan distance for the point
+                // is actually outside the current signal we are checking, since we're adding left and right
+                // whether or not the point is on the left and right, since top and bottom require both to be checked
+                if (PointInBounds(point,(0,maxSize)) &&
+                    CalculateManhattanDistance(distance.Key.row, distance.Key.col, point.row, point.col) > distance.Value)
+                {
+                    // Now that we know the point is one we actually want to check, compare against all the signals
+                    bool found = true;
+                    foreach (var signal in signals)
+                    {
+                        if (CalculateManhattanDistance(signal.Key.row, signal.Key.col, point.row, point.col) <= signal.Value)
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found)
+                    {
+                        // If the point is outside the manhattan distance for all signals, it's our answer. Quit early
+                        return point;
+                    }
+                }
             }
-            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col-col-1) > distance.Value)
-            {
-                possible.Add((distance.Key.row+row,distance.Key.col-col-1));
-            }
+
+            return null;
         }
     }
 }
