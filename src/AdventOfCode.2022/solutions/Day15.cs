b830c97a7cd43e1a5218f2c54c0f931bba2c32b4
Michelle McDaniel <adiaaida@gmail.com>
2022-12-14 23:39:42 -0800

    Fix up day 15 so it runs... well, not quite as slowly but still slowly.

--- src/AdventOfCode.2022/solutions/Day15.cs
+++ src/AdventOfCode.2022/solutions/Day15.cs
@@ -16,8 +16,6 @@ namespace AdventOfCode.TwentyTwo
 
             DictionaryMap<char> map = new();
             Dictionary<(int row, int col), int> manhattanDistances = new();
-            long minRow = long.MaxValue;
-            long maxRow = long.MinValue;
             long minCol = long.MaxValue;
             long maxCol = long.MinValue;
 
@@ -36,9 +34,6 @@ namespace AdventOfCode.TwentyTwo
                 map.TryAdd((beaconRow, beaconCol), 'B');
                 beacons.Add((beaconRow, beaconCol));
 
-                minRow = Math.Min(Math.Min(sensorRow, minRow), beaconRow);
-                maxRow = Math.Max(Math.Max(sensorRow, maxRow), beaconRow);
-
                 minCol = Math.Min(Math.Min(sensorCol, minCol), beaconCol);
                 maxCol = Math.Max(Math.Max(sensorCol, maxCol), beaconCol);
 
@@ -47,11 +42,9 @@ namespace AdventOfCode.TwentyTwo
             }
 
             long searchRow = 2000000;
-            //long row = 10;
-
-            Dictionary<(long, long), int> noBeacons = new();
             HashSet<(long, long)> beaconSet = new();
 
+            // This is super slow, but tbh, I have no idea what to do about it.
             foreach(var distance in manhattanDistances)
             {
                 for (long i = minCol-distance.Value; i < maxCol+distance.Value; i++)
@@ -72,34 +65,98 @@ namespace AdventOfCode.TwentyTwo
             }
 
             Console.WriteLine($"Part one: {beaconSet.Count()}");
-            HashSet<(long row, long col)> possibleLocations = new();
-
-            //long max = 20;
+            
+            HashSet<(long row, long col)> possible = new();
             long max = 4000000;
-            for (long row = 0; row < max; row++)
-            {
-                for(long col = 0; col < max; col++)
+
+            // Narrow down the search space a little, but it is still super slow
+            foreach (var distance in manhattanDistances)
+            { 
+                long row = 0;
+                long col = distance.Value;
+
+                while (row <= distance.Value && col >= 0)
                 {
-                    possibleLocations.Add((row,col));
-                    foreach (var distance in manhattanDistances)
-                    {
-                        if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, row, col) <= distance.Value)
-                        {
-                            possibleLocations.Remove((row,col));
-                            break;
-                        }
-                    }
-                    
+                    CheckNeighbors(possible, distance, row, col);
+
+                    col--;
+                    row++;
+                }
+
+                row = 0;
+                col = distance.Value;
+
+                // now check the down half
+                while (row >= -distance.Value && col >= 0)
+                {
+                    CheckNeighbors(possible, distance, row, col);
+
+                    col--;
+                    row--;
                 }
             }
 
-            Console.WriteLine(possibleLocations.First());
-            Console.WriteLine($"Part two: {possibleLocations.First().col*4000000 + possibleLocations.First().row}");
+            foreach (var poss in possible)
+            {
+                if (poss.row < 0 || poss.col < 0 || poss.row > max || poss.col > max)
+                {
+                    continue;
+                }
+                bool found = true;
+                foreach(var distance in manhattanDistances)
+                {
+                    if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, poss.row, poss.col) <= distance.Value)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine($"Part Two: {poss.row} {poss.col} = {poss.col*4000000 + poss.row}");
+                    break;
+                }
+            }
         }
 
         public static long CalculateManhattanDistance(long sensorRow, long sensorCol, long beaconRow, long beaconCol)
         {
             return Math.Abs(sensorRow - beaconRow)+Math.Abs(sensorCol - beaconCol);
+        }
+
+        public static void CheckNeighbors(HashSet<(long row, long col)> possible, KeyValuePair<(int row, int col), int> distance, long row, long col)
+        {
+            if (col == 0)
+            {
+                // gotta check up and down
+                if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row+1,distance.Key.col) > distance.Value)
+                {
+                    possible.Add((distance.Key.row+row+1, distance.Key.col));
+                }
+                if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row-1,distance.Key.col) > distance.Value)
+                {
+                    possible.Add((distance.Key.row+row-1, distance.Key.col));
+                }
+            }
+
+            // check left and right
+            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col+col+1) > distance.Value)
+            {
+                possible.Add((distance.Key.row+row,distance.Key.col+col+1));
+            }
+            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col+col-1) > distance.Value)
+            {
+                possible.Add((distance.Key.row+row,distance.Key.col+col-1));
+            }
+
+            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col-col+1) > distance.Value)
+            {
+                possible.Add((distance.Key.row+row,distance.Key.col-col+1));
+            }
+            if (CalculateManhattanDistance(distance.Key.row, distance.Key.col, distance.Key.row+row,distance.Key.col-col-1) > distance.Value)
+            {
+                possible.Add((distance.Key.row+row,distance.Key.col-col-1));
+            }
         }
     }
 }
